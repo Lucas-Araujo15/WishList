f60dabc61675eba4edf77754226f8a4e366efba6
Lucas Araujo <lucasaraujo4188@gmail.com>
2021-10-26 11:52:34 -0300

    Inicia a construção dos componentes

--- WishList/Front-End/wishlist-ui/src/pages/desejos/Desejos.jsx
+++ WishList/Front-End/wishlist-ui/src/pages/desejos/Desejos.jsx
@@ -1,8 +1,36 @@
+import { Component } from "react";
+
+class DesejosCadastro extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      idUsuario: 0,
+      descricaoDesejo: ''
+    }
+  };
+
+  cadastrar = () => {
+
+    fetch('http://localhost:5000/desejos', {
+      method: 'POST',
+      body: JSON.stringify({
+        idUsuario: this.state.idUsuario,
+        descricao: this.state.descricaoDesejo
+      }),
+
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+  }
+}
+
 function Desejos() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Página da lista de desejos</h1>
+        <DesejosCadastro />
       </header>
     </div>
   );
