80b2a9067f26870fa618eb99c476f3db86bb9b19
Lucas Araujo <lucasaraujo4188@gmail.com>
2021-10-26 11:05:13 -0300

    Configura as rotas - ReactJs

--- WishList/Front-End/wishlist-ui/src/index.js
+++ WishList/Front-End/wishlist-ui/src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import Home from './pages/home/App';
+import Desejos from './pages/desejos/Desejos.jsx'
+import NotFound from './pages/notFound/NotFound.jsx'
+
 import reportWebVitals from './reportWebVitals';
+import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
+
+
+const routing = (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/desejos" component={Desejos} />
+        <Route path="/notfound" component={NotFound} />
+        <Redirect to="/notfound" />
+      </Switch>
+    </div>
+  </Router>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  routing,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
---
+++ WishList/Front-End/wishlist-ui/src/pages/desejos/Desejos.jsx
@@ -0,0 +1,11 @@
+function Desejos() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Página da lista de desejos</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Desejos;
