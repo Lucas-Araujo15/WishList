e602bb3c005be145feabbf59d30462cda9f1b046
Lucas Araujo <lucasaraujo4188@gmail.com>
2021-10-27 14:11:33 -0300

    Adiciona página de cadastro de desejos

--- WishList/Front-End/wishlist-ui/src/pages/desejos/Desejos.jsx
+++ WishList/Front-End/wishlist-ui/src/pages/desejos/Desejos.jsx
@@ -1,11 +1,17 @@
-import { Component } from "react";
+import { Component } from 'react'
+import './style_Desejos.css'
+import logo from './assets/logo-wishlist.svg'
+import imagemPainel from './assets/painel-cenario.png'
+import genio from './assets/imagem-genio.png'
 
-class DesejosCadastro extends Component {
+class Desejos extends Component {
   constructor(props) {
     super(props);
     this.state = {
       idUsuario: 0,
-      descricaoDesejo: ''
+      descricaoDesejo: '',
+      listaDesejos: [],
+      pagina: false
     }
   };
 
@@ -23,15 +29,6 @@ class DesejosCadastro extends Component {
       }
     })
   }
-}
-
-class DesejoListagem extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      listaDesejos: []
-    }
-  }
 
   buscarDesejos = (event) => {
     event.preventDefault();
@@ -44,18 +41,45 @@ class DesejoListagem extends Component {
 
       .catch(erro => console.log(erro))
   }
-}
 
-function Desejos() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Página da lista de desejos</h1>
-        <button onClick={<DesejoListagem />}></button>
-        <button onClick={<DesejosCadastro />}></button>
-      </header>
-    </div>
-  );
+  abrirPagina = () => {
+    this.setState({
+      pagina: !this.state.pagina
+    })
+    console.log(this.state.pagina)
+  }
+
+  render() {
+    return (
+      <div className="app navegacao">
+        <aside className="abas">
+          <div className="container-abas">
+            <div>
+              <img className="logo" src={logo} alt="" />
+              <button onClick={this.abrirPagina} disabled={this.state.pagina === false ? true : false}>Cadastros</button>
+              <button onClick={this.abrirPagina} disabled={this.state.pagina !== false ? true : false}>Lista</button>
+            </div>
+            <img className="painel" src={imagemPainel} alt="" />
+          </div>
+        </aside>
+        <div style={this.state.pagina === false? {display:''} : {display: 'none'}} className="box-content">
+          <h1>Informe seus desejos e veja acontecer!</h1>
+          <div>
+            <form action="">
+              <h2>Diga-me, o que deseja?</h2>
+              <input className="input1" placeholder="quem deseja?" type="text" />
+              <textarea className="input2" placeholder="o desejo é..." name="" id="" cols="30" rows="10"></textarea>
+              <button>Cadastrar</button>
+            </form>
+            <img src={genio} alt="" />
+          </div>
+        </div >
+        <div style={this.state.pagina === false ? {display:'none'} : {display: ''}} className="listas">
+          <p>sfrghçsioderufbghçiozsdfgvçodsfghçiou</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Desejos;
